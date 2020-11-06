6b5c90a947213505d35939d1644ca7e317b5ee41
Eliezer Villegas <Ownerglif@gmail.com>
2020-11-06 00:18:14 -0700

    hola

---
+++ src/public/js/popup.js
@@ -0,0 +1,22 @@
+document.getElementById('publicar').addEventListener('click',
+function(){
+    document.querySelector('.post-view').style.display= 'flex';
+    document.querySelector('.publicar-p').style.display= 'none';
+});
+
+
+document.getElementById('confirmar').addEventListener('click',
+function(){
+    document.querySelector('.post-view').style.display= 'none';
+    document.querySelector('.publicar-p').style.display= 'flex';
+});
+
+
+
+//el hansen se la come
+//le pegaron gane con la mels JAJAJA
+//mucho chapulin
+//xd
+
+
+
